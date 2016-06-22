eee18e8ad01ef06d26a3bfe8bc8eb25f72f4af9a
pchaturvedi <pchaturvedi@prosper.com>
2016-06-21 21:21:56 -0700

    app

--- src/main/java/com/twtr/processor/TweetProcessor.java
+++ src/main/java/com/twtr/processor/TweetProcessor.java
@@ -17,19 +17,21 @@ public class TweetProcessor implements Runnable {
         this.tweetQueueManager = tweetQueueManager;
     }
 
-    
+    /**
+     * this method reads tweet from the queue
+     */
     public void run() {
         synchronized (tweetQueueManager) {
             while (true) {
                 if (!tweetQueueManager.isTweetQueueEmpty()) {
-                    LOGGER.debug("Extracting hashtags from message.");
+                    LOGGER.debug("Extracting hashtags from tweet queue.");
                     extractHashtagsFromTweet(tweetQueueManager.deQueueTweet());
                 } else {
-                    LOGGER.debug("The queue is empty. Waiting...");
+                    LOGGER.debug("No tweets in the queue, waiting...");
                     try {
                     	tweetQueueManager.wait();
                     } catch (InterruptedException ex) {
-                        LOGGER.error("InterruptedException thrown: " + ex);
+                        LOGGER.error("Thread interrupted: " + ex);
                         Thread.currentThread().interrupt();
                     }
                 }
@@ -37,8 +39,16 @@ public class TweetProcessor implements Runnable {
         }
     }
 
+    /**
+     * this method extracts the hashtag from each tweet, and adds it to the map
+     * @param tweet
+     */
     private void extractHashtagsFromTweet(final String tweet) {
-        String deliminator = " \t\n\r\f,.:;?![]'"; //adds punctuation marks to default set
+    	/**
+    	 * Tweet delimiter reference: https://dev.twitter.com/streaming/overview/processing
+    	 * String tokenizer reference: http://docs.oracle.com/javase/7/docs/api/java/util/StringTokenizer.html#StringTokenizer%28java.lang.String%29 
+    	 */
+        String deliminator = " \t\n\r\f,.:;?![]'";
         StringTokenizer tokenizer = new StringTokenizer(tweet, deliminator);
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
